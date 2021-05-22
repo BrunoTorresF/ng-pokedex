d8c835054ec233f8baa8cf7b947e1b31a785fc9c
BrunoTorresF <bruno.torres.fabila@gmail.com>
2021-05-22 14:07:42 -0500

    chore: push WSL changes

---
+++ src/app/components/pokemoncard/pokemoncard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemoncard',
+  templateUrl: './pokemoncard.component.html',
+  styleUrls: ['./pokemoncard.component.sass']
+})
+export class PokemoncardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
